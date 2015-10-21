faad5c95cce85b68272646f6722d27a1ac3c9463
Monarin Uervirojnangkoorn <monarin@gmail.com>
2015-10-21 21:31:02 +0000

    Parameter n_residues is now a required parameter.

--- postrefine/mod_input.py
+++ postrefine/mod_input.py
@@ -369,6 +369,12 @@ def process_input(argv=None):
                 "Oops, incorrect target_crystal_system (available options: Triclinic, Monoclinic, Orthorhombic, Tetragonal, Trigonal, Hexagonal, or Cubic)."
             )
 
+    # check n_residues
+    if params.n_residues is None:
+        raise Sorry(
+            "Oops, we have a new required parameter n_residues. Please specify number of residues of your structure in asymmetric unit (n_residues = xxx)."
+        )
+
     # check indexing ambiguity parameters
     if (
         params.indexing_ambiguity.flag_on
