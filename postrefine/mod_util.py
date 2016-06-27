2a8f5bffdff099ea0c92b4581e14388b5fcfaea4
Monarin Uervirojnangkoorn <monarin@gmail.com>
2016-06-27 06:28:02 +0000

    Add wavelenth to mtz file. Add prime.comparestats to compare stats in different runs.

--- postrefine/mod_util.py
+++ postrefine/mod_util.py
@@ -1108,6 +1108,8 @@ class intensities_scaler(object):
         if output_mtz_file_prefix != "":
             # write as mtz file
             miller_array_merge_unique = miller_array_merge.merge_equivalents().array()
+            info = miller.array_info(wavelength=wavelength_mean)
+            miller_array_merge_unique.set_info(info)
             mtz_dataset_merge = miller_array_merge_unique.as_mtz_dataset(
                 column_root_label="IOBS"
             )
