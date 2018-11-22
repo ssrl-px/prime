c7eb1847b73bfb33214e360eca7a33237266a31a
Artem Lyubimov <lyubimov@stanford.edu>
2018-11-21 17:57:09 -0800

    bugfixes to IOTA and PRIME GUIs
    PRIME:
        - PRIME now launches in independent window (i.e. won't be closed if IOTA is
          closed)
        - Made PRIME GUI wxPython4-compatible
        - Final result now written out into a subdirectory in the directory from
          which PRIME is launched
        - PRIME window launches on current display
    
    IOTA:
        - Fixed a bug that caused run resumption to fail
        - Abort command for LivePRIME in IOTA
        - Visualization base folder created at all times
        - Fixed 3D bug in standalone plotting window
        - Run number in title bar

--- command_line/prime_gui.py
+++ command_line/prime_gui.py
@@ -23,8 +23,7 @@ class MainApp(wx.App):
 
     def OnInit(self):
         self.frame = PRIMEWindow(None, -1, title="PRIME")
-        self.frame.SetPosition((150, 150))
-        self.frame.SetMinSize(self.frame.GetEffectiveMinSize())
+        self.frame.place_and_size(set_size=True, set_by="mouse")
         self.frame.Show(True)
         self.SetTopWindow(self.frame)
         return True
