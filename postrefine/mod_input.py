cda156b823ce73a69cbdd0d2cd919eb156b15be4
Artem Lyubimov <lyubimov@stanford.edu>
2019-06-07 14:04:37 -0700

    IOTA & PRIME GUI updates
    - streamlined plotters
    - bugfixes

--- postrefine/mod_input.py
+++ postrefine/mod_input.py
@@ -1,7 +1,6 @@
-"""read PRIME input."""
-from __future__ import absolute_import, division, print_function
-from six.moves import range
+from __future__ import division, print_function, absolute_import
 
+"""read PRIME input"""
 # Define exceptions
 class ReadInputError(Exception):
     pass
@@ -561,7 +560,7 @@ def read_pickles(data):
     # take care of tar files
     for tar_filename in frame_files:
         tarf = tarfile.open(name=tar_filename, mode="r")
-        for myindex in range(len(tarf.getmembers())):
+        for myindex in xrange(len(tarf.getmembers())):
             tar_files.append(tar_filename + ":ind" + str(myindex))
     return tar_files
 
